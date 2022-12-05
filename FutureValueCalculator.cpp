0b52ddb7cef6b3990667745ea39e9f3c8f154e2c
Timmurray <murrayt132@gmail.com>
2022-12-05 13:53:05 -0500

    future value program
    new program utilizing for loop

---
+++ FutureValueCalculator.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    cout << "The FUture Value Calculator\n\n";
+
+    char choice = 'y';
+    while(choice == 'y' || choice == 'Y') {
+        double monthly_investment;
+        cout << "enter Monthly investment:  ";
+        cin >> monthly_investment;
+
+        double yearly_rate;
+        cout << "Enter yearly interest rate:    ";
+        cin >> yearly_rate;
+
+        int years;
+        cout << "Enter number of years:     ";
+        cin >> years;
+
+        // convert yearly values to monthly values
+        double monthly_rate = yearly_rate / 12 / 100;
+        int months = years * 12;
+
+        //calculate future value
+        double future_value = 0;
+        for(int i = 1; i <= months; i++) {
+            future_value = (future_value + monthly_investment) * (1 + monthly_rate);
+        }
+
+        // round to 2 decimal places and display
+        future_value = round(future_value * 100) / 100;
+        cout << "future_value:          " << future_value << "\n\n";
+
+        //see if user wishes to ccontinue
+        cout << "Continue? (y/n): ";
+        cin >> choice;
+        cout << endl;
+    }
+    cout << "Have a good afternoon ";
+}
